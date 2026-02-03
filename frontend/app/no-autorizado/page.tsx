b81377698d7c3a20ac203be9d26a08cd8310e6ac
Facundo <facundomoriconi19@gmail.com>
2026-02-03 14:03:26 -0300

    commit

--- frontend/app/no-autorizado/page.tsx
+++ frontend/app/no-autorizado/page.tsx
@@ -1,7 +1,7 @@
 export default function NoAutorizado() {
   return (
     <div className="min-h-screen flex items-center justify-center text-red-600 text-xl font-semibold">
-      Acceso denegado. No estás autorizado.
+      Acceso denegado. No estás autorizado!.
     </div>
   )
 }
